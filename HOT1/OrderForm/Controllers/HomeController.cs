ec600144deef262150db075147b63e4d10a2ae3e
Amel_Topalovic <97201738+AmelTopalovic@users.noreply.github.com>
2023-01-23 22:39:02 -0600

    maybe this will be good for a B?

--- HOT1/OrderForm/Controllers/HomeController.cs
+++ HOT1/OrderForm/Controllers/HomeController.cs
@@ -8,11 +8,42 @@ namespace OrderForm.Controllers
 		[HttpGet]
 		public IActionResult Index()
 		{
+			ViewBag.quantity = 0;
+			ViewBag.discountCode = null;
+			ViewBag.pricePerShirt = 0.0;
+			ViewBag.subtotal = 0.0;
+			ViewBag.tax = 0.0;
+			ViewBag.total = 0.0;
+			ViewBag.summary = null;
+
 			return View();
 		}
 		[HttpPost]
 		public IActionResult Index(OrderFormModel model)
 		{
+			if (ModelState.IsValid)
+			{
+				ViewBag.quantity = model.Quantity;
+				ViewBag.discountCode = model.DiscountCode;
+				ViewBag.pricePerShirt = model.PricePerShirt;
+				ViewBag.discountMessage = model.DiscountMessage;
+				ViewBag.subtotal = model.Subtotal;
+				ViewBag.tax = model.Tax;
+				ViewBag.total = model.Total;
+				ViewBag.summary = model.Summary;
+
+			}
+			else
+			{
+				ViewBag.quantity = 0;
+				ViewBag.discountCode = null;
+				ViewBag.discountMessage = null;
+				ViewBag.pricePerShirt = 0.0;
+				ViewBag.subtotal = 0.0;
+				ViewBag.tax = 0.0;
+				ViewBag.total = 0.0;
+
+			}
 			return View(model);
 		}
 	}
--- HOT1/OrderForm/Models/OrderFormModel.cs
+++ HOT1/OrderForm/Models/OrderFormModel.cs
@@ -2,7 +2,89 @@
 namespace OrderForm.Models
 {
 	public class OrderFormModel
-	{
+
+
+	{	public string? _discountCode;
+
+		[Required(ErrorMessage = "You must enter a quantity!")]
+		public int? Quantity { get; set; }
+
+		public string? DiscountCode
+		{
+			get
+			{
+				return _discountCode;
+			}
+			set
+			{
+				_discountCode = value;
+				if (_discountCode == "6175")
+				{
+					DiscountMessage = "30% discount applied!";
+					PricePerShirt = PricePerShirt - (PricePerShirt * 0.30);
+				}
+				else if (_discountCode == "1390")
+				{
+					DiscountMessage = "20% discount applied!";
+					PricePerShirt = PricePerShirt - (PricePerShirt * 0.20);
+				}
+				else if (_discountCode == "BB88")
+				{
+					DiscountMessage = "10% discount applied!";
+					PricePerShirt = PricePerShirt - (PricePerShirt * 0.10);
+				}
+				else if (_discountCode == null)
+				{
+					DiscountMessage = string.Empty;
+				}
+				else
+				{
+					DiscountMessage = "* Invalid discount code!";
+				}
+			}
+		}
+
+
+
+		public string? DiscountMessage { get; set; }
+		
+		public double? PricePerShirt { get; set; } = 15.00;
+		public double? Subtotal
+		{
+			get
+			{
+				return Quantity * PricePerShirt;
+			}
+
+		}
+		public double? Tax {
+			get
+			{
+				return Subtotal * 0.08;
+			}
+		}
+		public double? Total
+		{
+			get
+			{
+				return Subtotal + Tax;
+			}
+
+
+		}
+
+		public string? Summary
+		{
+			get
+			{
+				return $"{Quantity} shirts @ {PricePerShirt:C2} each\n"+
+					$"-~-~-~-~-~-~-~-~-~-\n" +
+					$"Subtotal: {Subtotal:C2}\n\n" +
+					$"Tax: {Tax:C2}\n\n"+
+					$"Total: {Total:C2}";
+			}
+		}
+		
 		
 	}
 }
